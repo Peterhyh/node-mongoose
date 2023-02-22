702a837a294e39edae05f63735a67e3b4d2e3126
Peter Huynh <peterhyh@yahoo.com>
2023-02-22 17:08:37 -0600

    Mongoose Part 3

--- index.js
+++ index.js
@@ -1,38 +1,130 @@
+// const mongoose = require('mongoose');
+// const Campsite = require('./models/campsite');
+
+// //setting const for the url of the mongodb server
+// //this will automatically connect us to the nucampsitedb in the mongodb server.
+// const url = 'mongodb://localhost:27017/nucampsite';
+
+// //connect to the server:
+// const connect = mongoose.connect(url, {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+//the connect method returns a promise so now we can use the .then() method
+// connect.then(() => {            
+//     console.log('Connected correctly to server');
+//     
+// instatiate a new doc: const 
+//     newCampsite = new Campsite({
+//         name: "React Lake Campground",
+//         description: "test"
+//     });
+//         //using the save method on this new document to save to the campsite's collection in the database and return a promise:
+//         newCampsite.save()
+//         .then(campsite => {
+//             console.log(campsite);
+//             return Campsite.find();
+//         })
+//         .then(campsites => {
+//             console.log(campsites);
+//             return Campsite.deleteMany();
+//         })
+//         .then(() => {
+//             return mongoose.connection.close();
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             mongoose.connect.close();
+//         });
+// }); 
+
+
+//PART 2:
+// const mongoose = require('mongoose');
+// const Campsite = require('./models/campsite');
+
+// const url = 'mongodb://localhost:27017/nucampsite';
+
+// const connect = mongoose.connect(url, {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+// connect.then(() => {            
+//     console.log('Connected correctly to server');
+
+//     Campsite.create({
+//         name: "React Lake Campground",
+//         description: "test"
+//     })
+
+
+// dont need the save method anymore because Campsite.create() is able to save automatically.
+
+
+//     .then(campsite => {
+//         console.log(campsite);
+//         return Campsite.find();
+//     })
+//     .then(campsites => {
+//         console.log(campsites);
+//         return Campsite.deleteMany();
+//     })
+//     .then(() => {
+//         return mongoose.connection.close();
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         mongoose.connect.close();
+//     });
+// }); 
+
+
+//PART 3
 const mongoose = require('mongoose');
 const Campsite = require('./models/campsite');
 
-//setting const for the url of the mongodb server
-//this will automatically connect us to the nucampsitedb in the mongodb server.
 const url = 'mongodb://localhost:27017/nucampsite';
 
-//connect to the server:
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-connect.then(() => {            //the connect method returns a promise so now we can use the .then() method
+connect.then(() => {
     console.log('Connected correctly to server');
-    //PART 1
-    //instatiate a new doc: const 
-    //newCampsite = new Campsite
-    //PART 2
-    //we can use .create() which takes an argument an object that defines the new document to create, then automatically saves it. This create method returns a promise.
+
     Campsite.create({
         name: "React Lake Campground",
         description: "test"
     })
-        //PART 1
-        //using the save method on this new document to save to the campsite's collection in the database and return a promise:
-        //newCampsite.save()
-        //PART2 : dont need the save method anymore because Campsite.create() is able to save automatically.
         .then(campsite => {
             console.log(campsite);
-            return Campsite.find();
+            return Campsite.findByIdAndUpdate(campsite._id, {
+                $set: { description: 'Updated Test Document' }
+            }, {
+                new: true //this will cause this method to return the updated document
+            });
         })
-        .then(campsites => {
-            console.log(campsites);
+        //console logging the campsite document that was returned (original document, then a console log of the document with the updated description)
+        .then(campsite => {
+            console.log(campsite)
+
+
+            campsite.comments.push({
+                rating: 5,
+                text: 'What a magnificent view!',
+                author: "Tinus Lorvaldes"
+            });
+
+            return campsite.save(); //in order for this subcdocument update to take effect. this will return the campsite that was saved with the new comments subdocument. We will be receiving a single campsite document for the next .then()
+        })
+        .then(campsite => {
+            console.log(campsite);
             return Campsite.deleteMany();
         })
         .then(() => {
--- models/campsite.js
+++ models/campsite.js
@@ -1,8 +1,59 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
 //Creating the schema
 //Instatiate a new object called campsiteSchema
+// const campsiteSchema = new Schema({
+//     name: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     }
+//     //giving the schema a second optional argument which is used for setting various configuration options:
+// }, {
+//     timestamps: true //this will automatically add 2 properties to schema called, 'created at' and 'updated at'
+// });
+
+
+//creating the model using the schema:
+//mongoose.model returns a contructor function to instantiate objects
+//in this case this model will be used to instantiate documents for mongodb
+// const Campsite = mongoose.model('Campsite', campsiteSchema) //this creates a model named 'Campsite'. First argument must be capitalized, singular version of the collection that you want to use for this model.
+
+// module.exports = Campsite;
+
+//creating Node app to uses Mongoose and model&schema in campsite.js
+
+
+//Part 3
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//documents storing comments about a campsite
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -12,18 +63,18 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
-    //giving the schema a second optional argument which is used for setting various configuration options:
+    },
+    //adding subdocument inside the campsite schema 
+    //this will cause every campsite document to be able to contain multiple comment documents stored within an array
+    comments: [commentSchema]
 }, {
-    timestamps: true //this will automatically add 2 properties to schema called, 'created at' and 'updated at'
+    timestamps: true
 });
 
 
-//creating the model using the schema:
-//mongoose.model returns a contructor function to instantiate objects
-//in this case this model will be used to instantiate documents for mongodb
-const Campsite = mongoose.model('Campsite', campsiteSchema) //this creates a model named 'Campsite'. First argument must be capitalized, singular version of the collection that you want to use for this model.
+
+const Campsite = mongoose.model('Campsite', campsiteSchema)
 
 module.exports = Campsite;
 
-//creating Node app to uses Mongoose and model&schema in campsite.js
+
